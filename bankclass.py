45931456ae767f86353dff7c029b09062482159e
Prakash Dass R <rprakashdass@gmail.com>
2024-08-28 20:51:57 +0530

    bankclass edit

--- bankclass.py
+++ bankclass.py
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 """
-Author: 
-"""
-=======
-"""author- @ HARSHAD
+Author: @ HARSHAD
 """
 class Bank:
     def __init__(self, name):
         self.name = name
     def loan(self):
         return """this is the bankclass.py file"""
->>>>>>> 8bfbb7011cd814ca9af98f9c2ee700a13a36b2bf
